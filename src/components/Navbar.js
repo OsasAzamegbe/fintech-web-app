620ade635b6c4d6292c57e05138b5d5c3d685c55
osamudiamen <osas.azamegbe@gmail.com>
2020-10-03 13:04:57 +0100

    created Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-container container">
+                <Link to="/" className="navbar-logo" >
+                    Shinzu
+                </Link>
+            </div>
+        </nav>
+
+    )
+}
+
+export default Navbar
